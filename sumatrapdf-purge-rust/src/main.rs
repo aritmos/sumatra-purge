1667721f5b4c47ad17955529203c01ae9976f32d
aritmos <52433836+aritmos@users.noreply.github.com>
2023-10-14 23:48:01 +0100

    add rust implementation

--- sumatrapdf-purge-rust/src/main.rs
+++ sumatrapdf-purge-rust/src/main.rs
@@ -1,3 +1,42 @@
+use std::process::Command;
+
 fn main() {
-    println!("hello world")
+    let file = include_str!("settings.txt");
+    let mut lines = file.lines();
+    let mut pre_filestate_settings: Vec<String> = Vec::new();
+    loop {
+        let line = lines.next().unwrap();
+        pre_filestate_settings.push(line.to_string());
+        if line.starts_with("FileStates [") {
+            break;
+        }
+    }
+
+    let mut file_states: Vec<Vec<String>> = Vec::new();
+    let mut temp: Vec<String> = Vec::new();
+    for line in lines {
+        temp.push(line.to_string());
+        if line.starts_with("	]") {
+            file_states.push(temp.clone());
+            temp = Vec::new();
+        }
+    }
+
+    println!("Total: {} files", file_states.len());
+
+    let filtered_file_states: Vec<Vec<String>> = file_states.into_iter().filter(exists).collect();
+
+    println!("Total: {} existing files", filtered_file_states.len());
+}
+
+fn exists(file_state: &Vec<String>) -> bool {
+    let Some(windows_file_path) = &file_state[1].trim().strip_prefix("FilePath = ") else {
+        return false;
+    };
+
+    let Ok(child) = Command::new(format!("IF EXIST {windows_file_path} ECHO exists")).spawn() else {
+        return false;
+    };
+
+    child.stdout.is_some()
 }
